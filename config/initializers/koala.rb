6a0a56ab04659f86897dd1639aeba90eca483e00
Takao <>
2018-05-14 22:20:31 +0900

    facebook

---
+++ config/initializers/koala.rb
@@ -0,0 +1,7 @@
+# In Rails, you could put this in config/initializers/koala.rb
+Koala.configure do |config|
+  config.app_id = "2047855225428496"
+  config.app_secret = "bf61b31d7215fc020cc64749ba6846d5"
+  # See Koala::Configuration for more options, including details on how to send requests through
+  # your own proxy servers.
+end
